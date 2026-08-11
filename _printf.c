92a6f3d74f2aaf4b62aed0466894e8930f9c4f65
papadar <aladar@apponyi.net>
2026-08-11 00:29:10 +0000

    adding printf c file

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - print a string interspersed with other vars
+ * @format: pointer to the first character of the string
+ * Return: 0 if succesful, anything else is an error
+ */
+int _printf(const char *format, ...)
+{
+
+
+}
