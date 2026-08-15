d5ee501add0defc42cbb71cb08ef093d7af398ed
papadar <aladar@apponyi.net>
2026-08-15 15:34:24 +1000

    betty compliance and file consistency pass

--- printInt.c
+++ printInt.c
@@ -2,6 +2,38 @@
 #include "main.h"
 
 /**
+ * printIntRecursive - Recursive function to print digits.
+ * @n: The unsigned integer to print.
+ * Return: The number of digits printed.
+ */
+int printIntRecursive(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 10)
+	{
+		count += printIntRecursive(n / 10);
+	}
+
+	_putchar('0' + (n % 10));
+	return (count + 1);
+}
+
+/**
+ * printUSInt - Extracts an int from the va_list and prints it.
+ * @usintList: The argument list.
+ * Return: Total number of characters printed.
+ */
+int printUSInt(va_list usintList)
+{
+	int count = 0;
+	unsigned int num = va_arg(usintList, unsigned int);
+
+	count += printIntRecursive(num);
+	return (count);
+}
+
+/**
  * printInt - Extracts a +/- int from the va_list prints each digit
  * @intList: The argument list.
  * Return: Total number of characters printed (with minus sign if negative).
