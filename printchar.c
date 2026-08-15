7416bd4642a7238170b78a82a2d73a1f1545780e
papadar <aladar@apponyi.net>
2026-08-15 05:13:37 +0000

    updated print character function

--- printchar.c
+++ printchar.c
@@ -3,7 +3,18 @@
 #include <stdarg.h>
 #include "main.h"
 
+/**
+ * printChar - sends a character to standard output & returns a count
+ * @charList: the valist of arguments
+ * Return: a count of the total characters printed
+ * this should always be 1
+ */
+
 int printChar(va_list charList)
 {
-	
+	int count = 0;
+	char car = (char)va_arg(charList, int);
+
+	count = _putchar(car);
+	return (count);
 }
